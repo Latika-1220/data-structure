09b414983360343629f6bba4e3f284b1276e379c
Latika-1220 <latikasugavaneswaran20@gmail.com>
2025-08-03 09:47:29 +0530

    Update Stack.java

--- src/linear/Stack.java
+++ src/linear/Stack.java
@@ -1,46 +1,78 @@
 package linear;
+
 class SNode{
 	int data;
 	SNode next;
 	public SNode(int data) {
-		this.data=data;
+		this.data = data;
+		this.next = null;
 	}
 }
-public class Stack {
-	static SNode top,bottom;
-	static int count = -1;
-		public static void pop() {
-			if(count==-1) {
-				System.out.println("Stack is empty");
-				return;
-			}	
-			else {
-				SNode temp=bottom;
-				while(temp.next.next!=null) {
-					temp=temp.next;
-				}
-				System.out.println("Popped data"+top.data);
-				top=temp;
-				temp.next=null;
-				count--;
-				
-			}
+
+class StackOps{
+	static SNode top, bottom;
+	static int len=-1;
+	
+	public static void Push(int data) {
+		SNode node = new SNode(data);
+		if(top == null) {
+			top = node;
+			bottom = node;
+		}else {
+			top.next=node;
+			top = node;
 		}
-		public static void push(int data) {
-		SNode node=new SNode(data);
-		if(top==null) {
-			top=node;
+		len++;
+	}
+	
+	public static void Pop() {
+		if(len==-1) {
+			System.out.println("Stack Underflow");
+			return;
+		}else if(len==0){
+			System.out.println("Popped: "+top.data);
+			bottom = null;
+			len--;
 			return;
 		}
-		else {
-			top.next=node;
-			top=node;
+		{
+			System.out.println("Popped: "+top.data);
+			SNode temp = bottom;
+			while(temp.next.next!=null) {
+				temp = temp.next;
+			}
+			top = temp;
+			temp.next = null;
+		}
+		len--;
+	}
+	
+	public static void Peak() {
+		if(len==-1) {
+			System.out.println("Nothing to peak!");
+			return;
+		}else {
+			System.out.println("Toppest data: "+top.data);
 		}
 	}
-	public static void main(String[] args) {
-		push(5);
-		push(10);
-		pop();
-		pop();
+	
+	public static void display() {
+		SNode temp = bottom;
+		while(temp!=null) {
+			System.out.println(temp.data);
+			temp = temp.next;
+		}
 	}
+}
+
+public class Stack extends StackOps{
+	public static void main(String[] args) {
+		Push(2);
+		Push(3);
+		Push(4);
+		Push(6);
+		Peak();
+		display();
+	}
+
 }
